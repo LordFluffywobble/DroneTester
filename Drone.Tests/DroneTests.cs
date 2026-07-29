d26837938e4b008ad11c9040427a74c98cb0a543
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-07-29 13:10:17 +0200

    GREEN: Started movement testing for drone. Finding the new coordinates is working

---
+++ Drone.Tests/DroneTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Drone;
+
+namespace Drone.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void StartTest()
+    {
+        var drone = new Drone();
+        var result = drone.Start(6, 7, 5);
+        Assert.Equal(10.488088481701515, result);
+    }
+
+    [Fact]
+    public void NewPositionTest()
+    {
+        double[] arrayA = [5, 7, 8];
+        double[] arrayB = [3, 8, 2];
+        
+        var drone = new Drone();
+        var result = drone.NewPosition(arrayA, arrayB);
+        Assert.Equal([2, 15, 6], result);
+    }
+}
---
+++ Drone/Drone.cs
@@ -0,0 +1,27 @@
+
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Drone;
+
+public class Drone
+{
+    public double Start(double x, double y, double z) => 
+        Math.Sqrt((x*x) + (y*y) + (z*z));
+    
+
+    public double[] NewPosition(double[] arrayA, double[] arrayB)
+    {
+        for (int i = 0; i < arrayB.Length; i++)
+        {
+            if (arrayA[i] > arrayB[i])
+            {
+                arrayB[i] = -Math.Abs(arrayB[i]);
+            }
+        }
+
+        return arrayA.Zip(arrayB, (a,b) => a + b).ToArray();
+    }    
+
+}
